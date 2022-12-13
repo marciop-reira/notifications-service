5d854fcd9284984df1b4df6dd247f3d1e0d17342
Márcio Pereira <marciop.usa@gmail.com>
2022-12-12 22:15:06 -0300

    Create notification route and data validation

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,4 +1,6 @@
-import { Controller, Get } from '@nestjs/common';
+import { Body, Controller, Get, Post } from '@nestjs/common';
+import { randomUUID } from 'crypto';
+import { CreateNotificationBody } from './create-notification-body';
 import { PrismaService } from './prisma.service';
 
 @Controller('notifications')
@@ -6,7 +8,21 @@ export class AppController {
   constructor(private readonly prismaService: PrismaService) {}
 
   @Get()
-  getHello() {
-    return this.prismaService.notifications.findMany();
+  list() {
+    return this.prismaService.notification.findMany();
+  }
+
+  @Post()
+  async create(@Body() body: CreateNotificationBody) {
+    const { recipientId, content, category } = body;
+
+    await this.prismaService.notification.create({
+      data: {
+        id: randomUUID(),
+        recipientId,
+        content,
+        category,
+      },
+    });
   }
 }
